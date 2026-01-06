3b10f608bd84e6b705256855f7385993cc04a5cc
KazeAsh <pilgerashley@yahoo.com>
2026-01-06 12:15:05 +0900

    updated data_collector.py to collect weather data in japan.

---
+++ utils/data_collector.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import requests
+import cv2
+import os
+
+class DataCollector:
+    def __init__(self):
+        self.raw_data = "data/raw"
+        os.makedirs(self.raw_data, exist_ok=True)
+
+    #collect read data from weather in Japan
+    def collect_weather_data(self, city="Tokyo"):
+        # Using OpenWeatherMap API (free version)
+        api_key = "4178c9bc3ff08f945a51cb45dbf59dcf" #openweathermap.org
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            temp_kelvin = data["main"]["temp"] 
+            temp_celsius = temp_kelvin - 273.15
+
+            # Save as a CSV file
+            df = pd.DataFrame([{
+                "city": city,
+                "Time Stamp": pd.Timestamp.now(),
+                "temperature_celsius": temp_celsius,
+                "humidity": data["main"]["humidity"],
+                "weather": data["weather"][0]["description"],
+                "Source": "OpenWeatherMap"
+            }])
+            df.to_csv(os.path.join(self.raw_data, f"weather_{city}.csv"), index=False)
+            print(f"Weather data for {city} collected successfully.")
+
+            return df
+        return None
